c99e1da1e566afbc771f407318799f9a80e8e00c
Rashid Jilani <rashid.jilani@gmail.com>
2020-04-19 08:26:20 -0500

    grpc with maven example

---
+++ src/main/java/org/rashid/grpc/GrpcClient.java
@@ -0,0 +1,22 @@
+package org.rashid.grpc;
+
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.ManagedChannel;
+
+public class GrpcClient {
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 8080)
+                .usePlaintext()
+                .build();
+
+        HelloServiceGrpc.HelloServiceBlockingStub stub
+                = HelloServiceGrpc.newBlockingStub(channel);
+
+        HelloResponse helloResponse = stub.hello(HelloRequest.newBuilder()
+                .setFirstName("Baeldung")
+                .setLastName("gRPC")
+                .build());
+
+        channel.shutdown();
+    }
+}
